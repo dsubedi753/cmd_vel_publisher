6c123c66197b4ff538e257437317c88e65ba0eb7
dsubedi <divas.subedi@trincoll.edu>
2019-02-19 21:30:42 +0000

    Infinity_node

--- src/infinity_node.py
+++ src/infinity_node.py
@@ -4,23 +4,27 @@ import rospy
 import math
 from geometry_msgs.msg import Twist
 
-def sign(x):
-  if (x < 0):
-    return -1
-  return 1
-
 rospy.init_node('Infinity')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=6)
-rate = rospy.Rate(1)
-count = Twist()
-count.linear.x = 1
-count.linear.y = 0
-count.linear.z = 0
-count.angular.x = 0
-count.angular.y = 0
-s=1
+
+rate = rospy.Rate(10)
+mover = Twist()
+mover.linear.x = 1
+
+s=1 #Sign og angluar velocity
+print("Infinity Trajectory")
+
+while True:
+  t0 = rospy.Time.now().to_sec()
+  if t0 != 0: #initial time
+    break
 
 while not rospy.is_shutdown():
-  count.angular.z = s*0.5
-  pub.publish(count)
+  t1 = rospy.Time.now().to_sec()
+  t = t1-t0 #Elapsed time
+  if t >= 4*math.pi: #if the elapsed time is greater than equal to Time Period of 1 circle
+    s = -s
+    t0 = rospy.Time.now().to_sec()
+  mover.angular.z = s*0.5
+  pub.publish(mover)
   rate.sleep()
